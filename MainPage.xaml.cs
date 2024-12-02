d102d285773154b9eba6ced8c4d5c68e49e42d38
Cheboy03 <cornejoadrian93@gmail.com>
2024-12-02 10:37:45 -0500

    Diseño

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,44 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            var litros = Double.Parse(Litros.Text);
+     
+            var res = litros * 3.78;
+            ResLit.Text = res.ToString("F2"); 
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            var litrosMet = Double.Parse(LitrosMetros.Text);
+            var res1 = litrosMet * 1000;
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void Button_Clicked3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked6(object sender, EventArgs e)
+        {
+
         }
     }
 
